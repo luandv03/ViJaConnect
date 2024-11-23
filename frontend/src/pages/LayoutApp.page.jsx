cb89874f5c58736c4cc9c93942e8659ecb982553
Chu Đình Hiển <hienc1212@gmail.com>
2024-11-23 20:11:54 +0700

    Done header

--- frontend/src/pages/LayoutApp.page.jsx
+++ frontend/src/pages/LayoutApp.page.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
+import Header from "../components/Header";
 
 export default function LayoutApp() {
     return (
         <div>
-            <div className="w-full bg-red-400 h-20">Header</div>
+            {/* <div className="w-full bg-red-400 h-20">Header</div> */}
+            <Header />
             <div className="flex">
                 <div className="min-w-96 bg-slate-400">Sidebar</div>
 
