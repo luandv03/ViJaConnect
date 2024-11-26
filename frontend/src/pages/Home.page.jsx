d5ac8b801c947a8e281a60397505999cf6c2198b
Chu Đình Hiển <hienc1212@gmail.com>
2024-11-26 13:37:16 +0700

    Fake data

--- frontend/src/pages/Home.page.jsx
+++ frontend/src/pages/Home.page.jsx
@@ -1,19 +1,43 @@
 import { IconCirclePlus } from "@tabler/icons-react";
 
 import PostItem from "../components/Post/PostItem";
+import { Link } from "react-router-dom";
 
 function Home() {
+    const postItem = [
+        {
+            id: 1,
+            avatar: 'https://scontent.fhan15-2.fna.fbcdn.net/v/t39.30808-1/428297162_1760349381143905_2080811858875395246_n.jpg?stp=cp6_dst-jpg_s200x200_tt6&_nc_cat=100&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=z1fkJuv8C58Q7kNvgHBpSro&_nc_zt=24&_nc_ht=scontent.fhan15-2.fna&_nc_gid=AEqaVRD01ihiFdEqLyH_zHe&oh=00_AYBTEdAskxOmG1Oo8jkm_vYn79OQ6hC7XR-rFFq1-DbqHg&oe=67473A49',
+            img: 'https://pds.exblog.jp/pds/1/flash/top/image/e715a6283c881b09c58c1f1157ea0dcb.jpg',
+            name: 'ディン・ヴァン・ルアン',
+            date: '2024-12-20'
+        },
+        {
+            id: 2,
+            avatar: 'https://ca.slack-edge.com/T02QFU9TCTD-U050YRP1CKD-db63e56c3a98-512',
+            img: 'https://www.xserver.ne.jp/blog/wp-content/uploads/2020/05/how-to-write-blog-for-beginner-eyecatch.png',
+            name: 'グエン・ザ・トゥン・ゾオン',
+            date: '2024-12-20'
+        },
+        {
+            id: 3,
+            avatar: 'https://ca.slack-edge.com/T02QFU9TCTD-U05LS8WME6L-e9f842c7fcee-512',
+            img: 'https://www.xserver.ne.jp/blog/wp-content/uploads/2024/09/wordpress-xwrite-post-eyecatch.png',
+            name: 'グエン・ドゥック・フ',
+            date: '2024-12-20'
+        }
+    ]
     return (
         <>
             <div>
                 <div className="px-8 py-4">
-                    <div className="p-4 flex items-center space-x-10 bg-alice-blue rounded-lg mb-4">
+                    <Link to='/test' className="p-4 flex items-center space-x-10 bg-alice-blue rounded-lg mb-4 hover:text-gray-400">
                         <IconCirclePlus stroke={2} />
                         <div>新しいポストを作成する</div>
-                    </div>
+                    </Link>
                     <div>
-                        {Array.from({ length: 5 }, (_, i) => (
-                            <PostItem key={i} />
+                        { postItem.map((item) => (
+                            <PostItem key={item.id} post={item} />
                         ))}
                     </div>
                 </div>
